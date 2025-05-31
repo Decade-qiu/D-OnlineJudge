fdd35c948494d5c2cc5e79dcb96666378f9a1510
Decade-qiu <3237550600@qq.com>
2025-05-31 20:18:37 +0800

    :art: refactor: update problem-service configuration code

--- DOJ-BE/problem-service/src/main/java/com/decade/doj/problem/ProblemApplication.java
+++ DOJ-BE/problem-service/src/main/java/com/decade/doj/problem/ProblemApplication.java
@@ -3,6 +3,7 @@ package com.decade.doj.problem;
 import com.decade.doj.common.config.custom.JwtTool;
 import com.decade.doj.common.config.custom.MVCConfig;
 import com.decade.doj.common.config.custom.MybatisConfig;
+import com.decade.doj.common.interceptor.IdentityInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @MapperScan("com.decade.doj.problem.mapper")
-@Import({MVCConfig.class, MybatisConfig.class})
+@Import({MVCConfig.class, MybatisConfig.class, IdentityInterceptor.class})
 public class ProblemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProblemApplication.class, args);
