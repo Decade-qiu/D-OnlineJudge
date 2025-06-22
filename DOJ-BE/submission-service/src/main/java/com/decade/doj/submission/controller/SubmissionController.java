bbf5a953e2fe92f88c41daecd07a055c2762d04b
Decade-qiu <3237550600@qq.com>
2025-06-23 00:20:51 +0800

    :sparkles: feat: add submit status query

--- DOJ-BE/submission-service/src/main/java/com/decade/doj/submission/controller/SubmissionController.java
+++ DOJ-BE/submission-service/src/main/java/com/decade/doj/submission/controller/SubmissionController.java
@@ -2,6 +2,8 @@ package com.decade.doj.submission.controller;
 
 import com.decade.doj.common.domain.PageDTO;
 import com.decade.doj.common.domain.R;
+import com.decade.doj.common.domain.vo.ExecuteMessage;
+import com.decade.doj.common.utils.UserContext;
 import com.decade.doj.submission.domain.dto.SubmissionPageQueryDTO;
 import com.decade.doj.submission.domain.po.Submission;
 import com.decade.doj.submission.service.ISubmissionService;
@@ -10,6 +12,8 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/submission")
@@ -34,4 +38,24 @@ public class SubmissionController {
         return R.ok(res);
     }
 
+    @GetMapping("/match/{id}")
+    @ApiOperation("获取当前用户指定问题的提交详情")
+    public R<Integer> getById(@PathVariable String id) {
+        List<Submission> submissions = submissionService.lambdaQuery()
+                .eq(Submission::getProblemId, id)
+                .list();
+        int f = 0;
+        for (Submission submission : submissions) {
+            if (submission.getUserId() != null && submission.getUserId().equals(UserContext.getCurrentUser())) {
+                f = 1;
+                if (ExecuteMessage.getStatus(submission.getExitValue()).equals("Accepted")) {
+                    return R.ok(1); // 已经提交过且通过
+                }
+            }
+        }
+        if (f == 1) {
+            return R.ok(2); // 已经提交过但未通过
+        }
+        return R.ok(0);
+    }
 }
