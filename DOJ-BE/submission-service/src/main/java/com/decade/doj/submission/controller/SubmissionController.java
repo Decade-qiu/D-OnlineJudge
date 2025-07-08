c9eae70b4f48e02e6423f04885b22683cbf8661a
Decade <3237550600@qq.com>
2025-07-08 14:03:18 +0800

    :sparkles: feat(submission): add new submission service module

---
+++ DOJ-BE/submission-service/src/main/java/com/decade/doj/submission/controller/SubmissionController.java
@@ -0,0 +1,61 @@
+package com.decade.doj.submission.controller;
+
+import com.decade.doj.common.domain.PageDTO;
+import com.decade.doj.common.domain.R;
+import com.decade.doj.common.domain.vo.ExecuteMessage;
+import com.decade.doj.common.utils.UserContext;
+import com.decade.doj.submission.domain.dto.SubmissionPageQueryDTO;
+import com.decade.doj.submission.domain.po.Submission;
+import com.decade.doj.submission.service.ISubmissionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/submission")
+@Api(tags = "提交相关接口")
+@Slf4j
+@RequiredArgsConstructor
+public class SubmissionController {
+
+    private final ISubmissionService submissionService;
+
+    @PostMapping("/submit")
+    @ApiOperation("提交记录")
+    public R<String> submit(@RequestBody Submission submission) {
+        submissionService.save(submission);
+        return R.ok("提交成功");
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("分页获取提交列表")
+    public R<PageDTO<Submission>> page(SubmissionPageQueryDTO problemPageQueryDTO) {
+        PageDTO<Submission> res = submissionService.pageQuery(problemPageQueryDTO);
+        return R.ok(res);
+    }
+
+    @GetMapping("/match/{id}")
+    @ApiOperation("获取当前用户指定问题的提交详情")
+    public R<Integer> getById(@PathVariable String id) {
+        List<Submission> submissions = submissionService.lambdaQuery()
+                .eq(Submission::getProblemId, id)
+                .list();
+        int f = 0;
+        for (Submission submission : submissions) {
+            if (submission.getUserId() != null && submission.getUserId().equals(UserContext.getCurrentUser())) {
+                f = 1;
+                if (ExecuteMessage.getStatus(submission.getExitValue()).equals("Accepted")) {
+                    return R.ok(1); // 已经提交过且通过
+                }
+            }
+        }
+        if (f == 1) {
+            return R.ok(2); // 已经提交过但未通过
+        }
+        return R.ok(0);
+    }
+}
---
+++ DOJ-BE/submission-service/src/main/java/com/decade/doj/submission/service/impl/ISubmissionServiceImpl.java
@@ -0,0 +1,59 @@
+package com.decade.doj.submission.service.impl;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.decade.doj.common.client.ProblemClient;
+import com.decade.doj.common.client.SubmissionClient;
+import com.decade.doj.common.client.UserClient;
+import com.decade.doj.common.domain.PageDTO;
+import com.decade.doj.common.domain.po.Problem;
+import com.decade.doj.common.domain.po.User;
+import com.decade.doj.submission.domain.dto.SubmissionPageQueryDTO;
+import com.decade.doj.submission.domain.po.Submission;
+import com.decade.doj.submission.service.ISubmissionService;
+import com.decade.doj.submission.mapper.SubmissionMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.jdbc.Null;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+* @author qzj
+* @description 针对表【submission(代码提交记录表)】的数据库操作Service实现
+* @createDate 2025-06-05 13:28:52
+*/
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ISubmissionServiceImpl extends ServiceImpl<SubmissionMapper, Submission>
+    implements ISubmissionService {
+
+    @Override
+    public PageDTO<Submission> pageQuery(SubmissionPageQueryDTO submissionPageQueryDTO) {
+        log.info("分页查询提交列表: {}", submissionPageQueryDTO);
+        log.info("userId={}, problemId={}, status={}, language={}",
+                submissionPageQueryDTO.getUserId(),
+                submissionPageQueryDTO.getProblemId(),
+                submissionPageQueryDTO.getStatus(),
+                submissionPageQueryDTO.getLanguage());
+
+        String user = submissionPageQueryDTO.getUserId();
+        String problem = submissionPageQueryDTO.getProblemId();
+
+        Page<Submission> submissionList = lambdaQuery()
+                .like(user != null && !user.isBlank(), Submission::getUserName, submissionPageQueryDTO.getUserId())
+                .like(problem != null && !problem.isBlank(), Submission::getProblemName, submissionPageQueryDTO.getProblemId())
+                .eq(submissionPageQueryDTO.getStatus() != null, Submission::getStatus, submissionPageQueryDTO.getStatus())
+                .eq(submissionPageQueryDTO.getLanguage() != null, Submission::getLanguage, submissionPageQueryDTO.getLanguage())
+                .page(submissionPageQueryDTO.toMpPage("id", true));
+
+        return PageDTO.fullPage(submissionList.getTotal(), submissionList.getPages(), submissionList.getRecords());
+    }
+
+}
+
+
+
+
