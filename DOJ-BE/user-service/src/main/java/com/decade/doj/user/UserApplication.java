f81870eefc8a3dadc9cacacf36cc0b6978b51395
Decade-qiu <3237550600@qq.com>
2025-10-05 20:26:51 +0800

    :zap: feat(stats): implement async statistics update via RabbitMQ

--- DOJ-BE/user-service/src/main/java/com/decade/doj/user/UserApplication.java
+++ DOJ-BE/user-service/src/main/java/com/decade/doj/user/UserApplication.java
@@ -1,5 +1,6 @@
 package com.decade.doj.user;
 
+import com.decade.doj.common.config.custom.DefaultFeignConfig;
 import com.decade.doj.common.config.custom.JwtTool;
 import com.decade.doj.common.config.custom.MVCConfig;
 import com.decade.doj.common.config.custom.MybatisConfig;
@@ -8,10 +9,12 @@ import com.decade.doj.common.interceptor.IdentityInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @MapperScan("com.decade.doj.user.mapper")
+@EnableFeignClients(basePackages = "com.decade.doj.common.client", defaultConfiguration = DefaultFeignConfig.class)
 @Import({JwtTool.class, MVCConfig.class, MybatisConfig.class, IdentityInterceptor.class, AppNameProperties.class})
 public class UserApplication {
     public static void main(String[] args) {
