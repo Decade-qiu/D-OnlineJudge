eae444fa5f7cd888387f32eaab69119befc75825
Decade-qiu <3237550600@qq.com>
2025-10-07 00:11:31 +0800

    :sparkles: feat(sandbox): add judging task dto and mq config

--- DOJ-BE/sandbox-service/src/main/java/com/decade/doj/sandbox/domain/vo/JudgingTask.java
+++ DOJ-BE/sandbox-service/src/main/java/com/decade/doj/sandbox/domain/vo/JudgingTask.java
@@ -1,4 +1,18 @@
 package com.decade.doj.sandbox.domain.vo;
 
-public class judgingTask {
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class JudgingTask {
+    Long submissionId;
+    String localPath;
+    String input;
+    String output;
+    String filename;
+    String lang;
+    Long problemId;
+    String code;
+    Long uid;
 }
