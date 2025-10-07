d3b618d5e26d704c8498a88b48adbe0bccfbd175
Decade-qiu <3237550600@qq.com>
2025-10-07 22:19:42 +0800

    :bug: fix(mq): configure Jackson2JsonMessageConverter to fix deserialization issue

--- DOJ-BE/user-service/src/main/java/com/decade/doj/user/mq/MqConfig.java
+++ DOJ-BE/user-service/src/main/java/com/decade/doj/user/mq/MqConfig.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,5 +25,10 @@ public class MqConfig {
     @Bean
     public Binding binding() {
         return BindingBuilder.bind(statsUpdateQueue()).to(topicExchange()).with("problem.solved");
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 }
