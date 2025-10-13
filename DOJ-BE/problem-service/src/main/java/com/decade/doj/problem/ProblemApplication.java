c4f1b975a9fe5500d4fbe72d36d2a9f5da39bbf2
Decade-qiu <3237550600@qq.com>
2025-10-13 22:18:50 +0800

    :wrench: chore(problem): configure L1 and L2 cache managers

--- DOJ-BE/problem-service/src/main/java/com/decade/doj/problem/ProblemApplication.java
+++ DOJ-BE/problem-service/src/main/java/com/decade/doj/problem/ProblemApplication.java
@@ -7,10 +7,12 @@ import com.decade.doj.common.interceptor.IdentityInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @MapperScan("com.decade.doj.problem.mapper")
+@EnableCaching
 @Import({MVCConfig.class, MybatisConfig.class, IdentityInterceptor.class})
 public class ProblemApplication {
     public static void main(String[] args) {
